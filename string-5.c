86e22ada9531261634affc6763b55a8f6ad7cf93
Your Name <youremail@domain.com>
2021-11-13 20:47:05 +0500

    5-misol

---
+++ string-5.c
@@ -0,0 +1,17 @@
+   #include <stdio.h>
+
+
+ int main(){
+
+     char c=122;
+     int n;
+     printf("Enter n: ");
+     scanf("%d", &n);
+
+     for(int i=0; i<=n; i++){
+         c--;
+         printf("Natija %c\n", c);
+         }
+
+         return 0;
+ }
