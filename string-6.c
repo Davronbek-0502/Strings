f3e8114d0dcd4fafba92513dc41060cac092df5d
Your Name <youremail@domain.com>
2021-11-13 21:18:32 +0500

    6-misol

---
+++ string-6.c
@@ -0,0 +1,22 @@
+   #include <stdio.h>
+int main(){
+    char c;
+
+    printf("Enter n : ");
+
+    scanf("%c", &c);
+
+    if(((c >= 65)&&(c<= 90)) || ((c >= 97)&&(c <= 127))){
+        printf("lotin\n");
+    } 
+    if((c>=48)&&(c<=57)){
+        printf("digit");
+    }
+    else {
+        printf("0\n");
+    }
+
+
+
+  return 0;
+}
